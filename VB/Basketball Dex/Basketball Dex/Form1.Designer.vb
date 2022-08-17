4e78aac2f05561c961072f38dc6173a041a49ac0
Blake <bwwol@learner.dvusd.org>
2022-08-17 08:26:33 -0700

    Working Version

---
+++ VB/Basketball Dex/Basketball Dex/Form1.Designer.vb
@@ -0,0 +1,303 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Centerpicturebox = New System.Windows.Forms.PictureBox()
+        Me.Powerforwardpicturebox = New System.Windows.Forms.PictureBox()
+        Me.Smallforwardpicturebox = New System.Windows.Forms.PictureBox()
+        Me.Shootingguardpicturebox = New System.Windows.Forms.PictureBox()
+        Me.PointguardPictureBox = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.sizeLabel = New System.Windows.Forms.Label()
+        Me.shootingLabel = New System.Windows.Forms.Label()
+        Me.speedLabel = New System.Windows.Forms.Label()
+        Me.ballLabel = New System.Windows.Forms.Label()
+        Me.nameLabel = New System.Windows.Forms.Label()
+        CType(Me.Centerpicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Powerforwardpicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Smallforwardpicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Shootingguardpicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PointguardPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(398, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Name: "
+        '
+        'Centerpicturebox
+        '
+        Me.Centerpicturebox.Image = Global.Basketball_Dex.My.Resources.Resources.download__2_
+        Me.Centerpicturebox.Location = New System.Drawing.Point(443, 585)
+        Me.Centerpicturebox.Name = "Centerpicturebox"
+        Me.Centerpicturebox.Size = New System.Drawing.Size(100, 50)
+        Me.Centerpicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Centerpicturebox.TabIndex = 5
+        Me.Centerpicturebox.TabStop = False
+        '
+        'Powerforwardpicturebox
+        '
+        Me.Powerforwardpicturebox.Image = Global.Basketball_Dex.My.Resources.Resources.i
+        Me.Powerforwardpicturebox.Location = New System.Drawing.Point(337, 585)
+        Me.Powerforwardpicturebox.Name = "Powerforwardpicturebox"
+        Me.Powerforwardpicturebox.Size = New System.Drawing.Size(100, 50)
+        Me.Powerforwardpicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Powerforwardpicturebox.TabIndex = 4
+        Me.Powerforwardpicturebox.TabStop = False
+        '
+        'Smallforwardpicturebox
+        '
+        Me.Smallforwardpicturebox.Image = Global.Basketball_Dex.My.Resources.Resources.download__1_
+        Me.Smallforwardpicturebox.Location = New System.Drawing.Point(231, 585)
+        Me.Smallforwardpicturebox.Name = "Smallforwardpicturebox"
+        Me.Smallforwardpicturebox.Size = New System.Drawing.Size(100, 50)
+        Me.Smallforwardpicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Smallforwardpicturebox.TabIndex = 3
+        Me.Smallforwardpicturebox.TabStop = False
+        '
+        'Shootingguardpicturebox
+        '
+        Me.Shootingguardpicturebox.Image = Global.Basketball_Dex.My.Resources.Resources.download
+        Me.Shootingguardpicturebox.Location = New System.Drawing.Point(125, 585)
+        Me.Shootingguardpicturebox.Name = "Shootingguardpicturebox"
+        Me.Shootingguardpicturebox.Size = New System.Drawing.Size(100, 50)
+        Me.Shootingguardpicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Shootingguardpicturebox.TabIndex = 2
+        Me.Shootingguardpicturebox.TabStop = False
+        '
+        'PointguardPictureBox
+        '
+        Me.PointguardPictureBox.Image = Global.Basketball_Dex.My.Resources.Resources.stephen_curry
+        Me.PointguardPictureBox.Location = New System.Drawing.Point(10, 585)
+        Me.PointguardPictureBox.Name = "PointguardPictureBox"
+        Me.PointguardPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.PointguardPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PointguardPictureBox.TabIndex = 0
+        Me.PointguardPictureBox.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(10, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(367, 254)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(51, 440)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(92, 23)
+        Me.Button1.TabIndex = 11
+        Me.Button1.Text = "PointGuard"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(200, 440)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(122, 23)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "ShootingGuard"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(369, 439)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(121, 24)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "SmallForward"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(530, 439)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(113, 23)
+        Me.Button4.TabIndex = 14
+        Me.Button4.Text = "PowerForward"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(710, 439)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 15
+        Me.Button5.Text = "Center"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(395, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(36, 16)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "Size:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(395, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 16)
+        Me.Label3.TabIndex = 17
+        Me.Label3.Text = "Shooting"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(395, 115)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 16)
+        Me.Label4.TabIndex = 18
+        Me.Label4.Text = "Speed"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(398, 146)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(92, 16)
+        Me.Label5.TabIndex = 19
+        Me.Label5.Text = "Ball handeling"
+        '
+        'sizeLabel
+        '
+        Me.sizeLabel.AutoSize = True
+        Me.sizeLabel.Location = New System.Drawing.Point(467, 44)
+        Me.sizeLabel.Name = "sizeLabel"
+        Me.sizeLabel.Size = New System.Drawing.Size(0, 16)
+        Me.sizeLabel.TabIndex = 20
+        '
+        'shootingLabel
+        '
+        Me.shootingLabel.AutoSize = True
+        Me.shootingLabel.Location = New System.Drawing.Point(466, 75)
+        Me.shootingLabel.Name = "shootingLabel"
+        Me.shootingLabel.Size = New System.Drawing.Size(0, 16)
+        Me.shootingLabel.TabIndex = 21
+        '
+        'speedLabel
+        '
+        Me.speedLabel.AutoSize = True
+        Me.speedLabel.Location = New System.Drawing.Point(467, 115)
+        Me.speedLabel.Name = "speedLabel"
+        Me.speedLabel.Size = New System.Drawing.Size(0, 16)
+        Me.speedLabel.TabIndex = 22
+        '
+        'ballLabel
+        '
+        Me.ballLabel.AutoSize = True
+        Me.ballLabel.Location = New System.Drawing.Point(508, 146)
+        Me.ballLabel.Name = "ballLabel"
+        Me.ballLabel.Size = New System.Drawing.Size(0, 16)
+        Me.ballLabel.TabIndex = 23
+        '
+        'nameLabel
+        '
+        Me.nameLabel.AutoSize = True
+        Me.nameLabel.Location = New System.Drawing.Point(471, 9)
+        Me.nameLabel.Name = "nameLabel"
+        Me.nameLabel.Size = New System.Drawing.Size(0, 16)
+        Me.nameLabel.TabIndex = 24
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1032, 647)
+        Me.Controls.Add(Me.nameLabel)
+        Me.Controls.Add(Me.ballLabel)
+        Me.Controls.Add(Me.speedLabel)
+        Me.Controls.Add(Me.shootingLabel)
+        Me.Controls.Add(Me.sizeLabel)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Centerpicturebox)
+        Me.Controls.Add(Me.Powerforwardpicturebox)
+        Me.Controls.Add(Me.Smallforwardpicturebox)
+        Me.Controls.Add(Me.Shootingguardpicturebox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PointguardPictureBox)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.Centerpicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Powerforwardpicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Smallforwardpicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Shootingguardpicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PointguardPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PointguardPictureBox As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Shootingguardpicturebox As PictureBox
+    Friend WithEvents Smallforwardpicturebox As PictureBox
+    Friend WithEvents Powerforwardpicturebox As PictureBox
+    Friend WithEvents Centerpicturebox As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents sizeLabel As Label
+    Friend WithEvents shootingLabel As Label
+    Friend WithEvents speedLabel As Label
+    Friend WithEvents ballLabel As Label
+    Friend WithEvents nameLabel As Label
+End Class
---
+++ VB/Basketball Dex/Basketball Dex/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PictureBox1.Image = PointguardPictureBox.Image
+        nameLabel.Text = "Point Guard"
+        sizeLabel.Text = "A Point Guard is usually a little bit smaller."
+        shootingLabel.Text = "A point Gaurd is always a very good shooter."
+        speedLabel.Text = "A point Guard is almost always very fast."
+        ballLabel.Text = "A Point Guard must have great ball handeling skills."
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        PictureBox1.Image = Shootingguardpicturebox.Image
+        nameLabel.Text = "Shooting Guard"
+        sizeLabel.Text = "A Shooting Guard is usually pretty small."
+        shootingLabel.Text = "A Shooting Guard is always a great shooter and shoot the ball the most."
+        speedLabel.Text = "A Shooting Gaurd is usually pretty fast but not the fastest."
+        ballLabel.Text = "A Shooting Gaurd has good ball handeling skills a magority of the time."
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        PictureBox1.Image = Smallforwardpicturebox.Image
+        nameLabel.Text = "Small Forward"
+        sizeLabel.Text = "Small Forwards are usally very big when it comes to size."
+        shootingLabel.Text = "Small Forwads don't shoot as many threes but are very good at midrange."
+        speedLabel.Text = "Small Forwards are usually pretty fast but not the fastest."
+        ballLabel.Text = "Small Forwards can have good ball handeling but not usually."
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        PictureBox1.Image = Powerforwardpicturebox.Image
+        nameLabel.Text = "Power Forward"
+        sizeLabel.Text = "A Power Forward is huge and are some of the biggest on the court."
+        shootingLabel.Text = "Power Forwards are never good shooters and don't shoot very much."
+        speedLabel.Text = "Power Forwards aren't very fast because of how big they are."
+        ballLabel.Text = "Power Forwards don't have very great handles because of how lengthy they are."
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        PictureBox1.Image = Centerpicturebox.Image
+        nameLabel.Text = "Center"
+        sizeLabel.Text = "Centers are always the biggest on the court when it comes to size."
+        shootingLabel.Text = "Centers are never good at shooting because they play close to the hoop."
+        speedLabel.Text = "Centers are pretty slow because of how big they are."
+        ballLabel.Text = "Centers don't usually have great ball handeling skills."
+    End Sub
+
+End Class
